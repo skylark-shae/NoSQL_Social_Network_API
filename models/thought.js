ee67aa5d286bcf2689f22b10256eb68190b6b17e
Skylar Kline <skylarkline16@gmail.com>
2025-01-30 09:38:10 -0800

    models/Thoughts.js completed but not tested

--- models/thought.js
+++ models/thought.js
@@ -10,6 +10,37 @@ const ReactionSchema = new Schema({
     username: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
   });
-  
+
 
 // Thought references User for population:
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: { virtuals: true },
+    id: false,
+  }
+);
+
+// Create model using ThoughtSchema:
+const Thought = model('Thought', ThoughtSchema);
+
+// Export Thought model:
+module.exports = Thought;

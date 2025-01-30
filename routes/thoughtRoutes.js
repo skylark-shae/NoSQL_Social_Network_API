bc33a740412b16777933fd03f889092d12d2093a
Skylar Kline <skylarkline16@gmail.com>
2025-01-30 09:41:55 -0800

    routes/thoughtsRoutes.js completed but not tested

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -10,11 +10,27 @@ const {
 } = require('../controllers/thoughtController');
 
 // /api/thoughts
+// :thoughts
+router
+  .route('/')
+  .get(getThoughts)
+  .post(createThought);
 
-// /api/thoughts/:thoughtId
+// :thoughtId
+router
+  .route('/:thoughtId')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
 
-// /api/thoughts/:thoughtId/reactions
+// :thoughtId/reactions
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction);
 
-// /api/thoughts/:thoughtId/reactions/:reactionId
+// :thoughtId/reactions/:reactionId
+router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
 
 module.exports = router;

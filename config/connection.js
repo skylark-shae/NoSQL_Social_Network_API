5af9536545944749e6e18a85fb7977601847713f
Skylar Kline <skylarkline16@gmail.com>
2025-01-30 18:16:14 -0800

    updated code

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialNetworkDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialNetworkDB');
 
 mongoose.set('debug', false);
 
 module.exports = mongoose.connection;
+
+// Depreciated code from assistance
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialNetworkDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// mongoose.set('debug', false);
+
+// module.exports = mongoose.connection;
